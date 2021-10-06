937ffdb32e99c60f6406e30742809d91ad97c3c4
cderbignyv <91974361+cderbignyv@users.noreply.github.com>
2021-10-06 08:24:24 -0500

    answers 1-3 working on 4-10 for the next push

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { of } from 'rxjs';
+import { combineLatest, of } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 const streamOfNumbers = of(1,2,3,4,5,6, 6, 1, 5);
 const singleOutput = of([1,2,3]);
@@ -6,25 +7,52 @@ const singleOutput = of([1,2,3]);
 
 // Challenge 1 use streamOfNumbers and square each value [Easy]
 
+// streamOfNumbers.pipe(map((x) => x * x)).subscribe((x) => console.log(x));
 
 // Challenge 2 use streamOfNumbers and square only the even numbers [Easy]
 
+// streamOfNumbers.pipe(
+//     filter((x) =>{
+//         return x % 2 == 0;
+//     }),
+//     map((x) => x * x)).subscribe((x) => console.log(x));
 
 // Challenge 3 use singleOutput  and console log the value [Easy]
 
+// singleOutput.subscribe((x) => console.log(x));
 
 // Challenge 4 take the lastest from streamOfNumbers and singleOutput and multiply
 // the latest value from single output by the value returned from stream of numbers [Medium]
 
+streamOfNumbers.pipe(
+    filter((val) => {
+        return val % 2 == 0;
+    }),
+    map((x) => x * x)).subscribe((x) => console.log(x));
+        //val = 4, 16, 36, 36
 
+    singleOutput.subscribe((x) => console.log(x));
+
+const values = combineLatest(
+    [streamOfNumbers, singleOutput
+    ]).pipe(
+        map(([x, y]) => x * y),
+);
+
+values.subscribe(x, y => console.log('values is ' + x + y));
 // Challenge 5 take the latest from both observables anytime one of the obervables emits [Medium]
-// console log the emission. Be able to explain the output. 
+// console log the emission. Be able to explain the output.
+
+
+
 
 // Chanllenge 6 An error is going to occur with the below code. Prevent the error from stopping the stream [Medium]
 
     // of('a', 1, "b").pipe(map((x: any) => x.toUppercase())).subscribe((val) =>console.log(val));
 
-// Challenge 7 SwitchMap challenge be able to explain the output [Medium]
+
+
+    // Challenge 7 SwitchMap challenge be able to explain the output [Medium]
 // const character = new Observable((observer) => {
 //     observer.next('A');
 //     setTimeout(() => observer.next('B'), 200);
@@ -37,6 +65,11 @@ const singleOutput = of([1,2,3]);
 //     tap((x) => console.log(`value from source: ${x}`)),
 //     switchMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
+
+
+
+
+
 
 // Challenge 8 MergeMap challenge be able to explain the output and the difference from merge map [Medium]
 // const character = new Observable((observer) => {
@@ -52,7 +85,11 @@ const singleOutput = of([1,2,3]);
 //     mergeMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
 
+
+
 // Challenge 9 use streamOfNumbers and only emit values when they are different from the previous value. [Easy]
+
+
 
 // Challenge 10 use streamOfNumbers but stop taking values after the number 4 has been emitted [Easy]
 
