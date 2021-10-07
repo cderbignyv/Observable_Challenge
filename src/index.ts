adb20f65a38382f522c4e1e5ed80d2030ca9ce90
cderbignyv <91974361+cderbignyv@users.noreply.github.com>
2021-10-07 10:08:40 -0500

    finished up challenge 9 & 10

--- src/index.ts
+++ src/index.ts
@@ -7,60 +7,51 @@ const singleOutput = of([1,2,3]);
 
 // Challenge 1 use streamOfNumbers and square each value [Easy]
 
-// streamOfNumbers.pipe(map((x) => x * x)).subscribe((x) => console.log(x));
+streamOfNumbers.pipe(map((x) => x * x)).subscribe((x) => console.log(x));
 
 // Challenge 2 use streamOfNumbers and square only the even numbers [Easy]
 
-// streamOfNumbers.pipe(
-//     filter((x) =>{
-//         return x % 2 == 0;
-//     }),
-//     map((x) => x * x)).subscribe((x) => console.log(x));
+    streamOfNumbers.pipe(
+        filter((x) =>{
+            return x % 2 == 0;
+        }),
+    map((x) => x * x)).subscribe((x) => console.log(x));
 
 // Challenge 3 use singleOutput  and console log the value [Easy]
 
-// singleOutput.subscribe((x) => console.log(x));
+    singleOutput.subscribe((x) => console.log(x));
 
 // Challenge 4 take the lastest from streamOfNumbers and singleOutput and multiply
 // the latest value from single output by the value returned from stream of numbers [Medium]
 
-// streamOfNumbers.pipe(
-//     tap((val: number) => { console.log(val) }),
-//     withLatestFrom(singleOutput),
-//     map(([stream, single]) => {
-//         return  (`${stream} * ${single}`);
-//     })).subscribe((val) => console.log(val));
-
-// values.subscribe(x => console.log('values is ' + x));
+    streamOfNumbers.pipe(
+        tap((val: number) => { console.log(val) }),
+        withLatestFrom(singleOutput),
+        map(([stream, single]) => {
+            return single.map((test)=> test * stream)
+    })).subscribe((val) => console.log(val));
 
 // Challenge 5 take the latest from both observables anytime one of the obervables emits [Medium]
 // console log the emission. Be able to explain the output.
 
-// const connectStream = combineLatest([streamOfNumbers, singleOutput]).pipe(
-//     map(([stream, singleOut]) => {
-//         return (`${singleOut}`);
-//     }),
-//     connectStream.subscribe(([val]) => console.log(val))));
-
-
-
+    combineLatest([streamOfNumbers, singleOutput]).subscribe(([val]) => console.log(val));
 
 // Chanllenge 6 An error is going to occur with the below code. Prevent the error from stopping the stream [Medium]
     // of('a', 1, "b").pipe(map((x: any) => x.toUppercase())).subscribe((val) =>console.log(val));
 
-    // const letters$ = of('a', 1, "b");
-    // const upperCase$ = letters$.pipe(map((x: any) => {
-    //     try {
-    //         return x.toUpperCase();
-    //     } catch(error) {
-    //         return undefined;
-    //     }
-    // }));
-    // upperCase$.subscribe(
-    //     (val => console.log('Success: ', val)),
-    //     (val => console.log('Error: ', val)),
-    //     (() => console.log('Complete'))
-    // );
+    const letters$ = of('a', 1, "b");
+    const upperCase$ = letters$.pipe(map((x: any) => {
+        try {
+            return x.toUpperCase();
+        } catch(error) {
+            return undefined;
+        }
+    }));
+    upperCase$.subscribe(
+        (val => console.log('Success: ', val)),
+        (val => console.log('Error: ', val)),
+        (() => console.log('Complete'))
+    );
 
 // Challenge 7 SwitchMap challenge be able to explain the output [Medium]
 // const character = new Observable((observer) => {
@@ -76,10 +67,10 @@ const singleOutput = of([1,2,3]);
 //     switchMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
 
-//This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
-//and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
-//time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
-//observer and starts emiting values again starting from zero but continues until its complete.
+This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
+and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
+time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
+observer and starts emiting values again starting from zero but continues until its complete.
 
 // Challenge 8 MergeMap challenge be able to explain the output and the difference from switch map [Medium]
 // const character = new Observable((observer) => {
@@ -95,17 +86,17 @@ const singleOutput = of([1,2,3]);
 //     mergeMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
 
-//This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
-//and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
-//time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
-//observer and picks up were the first observer left off while starting the next observer, emiting
-//values until its complete.
+This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
+and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
+time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
+observer and picks up were the first observer left off while starting the next observer, emiting
+values until its complete.
 
 // Challenge 9 use streamOfNumbers and only emit values when they are different from the previous value. [Easy]
 
-// streamOfNumbers.pipe(distinct()).subscribe(x => console.log(x));
+streamOfNumbers.pipe(distinct()).subscribe(x => console.log(x));
 
 // Challenge 10 use streamOfNumbers but stop taking values after the number 4 has been emitted [Easy]
 
-// const untilFour = streamOfNumbers.pipe(take(4));
-// untilFour.subscribe(x => console.log(x));
+const untilFour = streamOfNumbers.pipe(take(4));
+untilFour.subscribe(x => console.log(x));
