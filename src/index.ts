faae4351c0bb4f7a9a784c5031180f8bc9b7ab39
cderbignyv <91974361+cderbignyv@users.noreply.github.com>
2021-10-07 09:22:32 -0500

    4-8 completed and part of 9 & 10 almost complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { combineLatest, of } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { combineLatest, from, interval, Observable, of } from 'rxjs';
+import { map, filter, distinct, take, tap, switchMap, mergeMap, withLatestFrom } from 'rxjs/operators';
 
 const streamOfNumbers = of(1,2,3,4,5,6, 6, 1, 5);
 const singleOutput = of([1,2,3]);
@@ -24,35 +24,45 @@ const singleOutput = of([1,2,3]);
 // Challenge 4 take the lastest from streamOfNumbers and singleOutput and multiply
 // the latest value from single output by the value returned from stream of numbers [Medium]
 
-streamOfNumbers.pipe(
-    filter((val) => {
-        return val % 2 == 0;
-    }),
-    map((x) => x * x)).subscribe((x) => console.log(x));
-        //val = 4, 16, 36, 36
+// streamOfNumbers.pipe(
+//     tap((val: number) => { console.log(val) }),
+//     withLatestFrom(singleOutput),
+//     map(([stream, single]) => {
+//         return  (`${stream} * ${single}`);
+//     })).subscribe((val) => console.log(val));
 
-    singleOutput.subscribe((x) => console.log(x));
+// values.subscribe(x => console.log('values is ' + x));
 
-const values = combineLatest(
-    [streamOfNumbers, singleOutput
-    ]).pipe(
-        map(([x, y]) => x * y),
-);
-
-values.subscribe(x, y => console.log('values is ' + x + y));
 // Challenge 5 take the latest from both observables anytime one of the obervables emits [Medium]
 // console log the emission. Be able to explain the output.
+
+// const connectStream = combineLatest([streamOfNumbers, singleOutput]).pipe(
+//     map(([stream, singleOut]) => {
+//         return (`${singleOut}`);
+//     }),
+//     connectStream.subscribe(([val]) => console.log(val))));
 
 
 
 
 // Chanllenge 6 An error is going to occur with the below code. Prevent the error from stopping the stream [Medium]
-
     // of('a', 1, "b").pipe(map((x: any) => x.toUppercase())).subscribe((val) =>console.log(val));
 
+    // const letters$ = of('a', 1, "b");
+    // const upperCase$ = letters$.pipe(map((x: any) => {
+    //     try {
+    //         return x.toUpperCase();
+    //     } catch(error) {
+    //         return undefined;
+    //     }
+    // }));
+    // upperCase$.subscribe(
+    //     (val => console.log('Success: ', val)),
+    //     (val => console.log('Error: ', val)),
+    //     (() => console.log('Complete'))
+    // );
 
-
-    // Challenge 7 SwitchMap challenge be able to explain the output [Medium]
+// Challenge 7 SwitchMap challenge be able to explain the output [Medium]
 // const character = new Observable((observer) => {
 //     observer.next('A');
 //     setTimeout(() => observer.next('B'), 200);
@@ -66,12 +76,12 @@ values.subscribe(x, y => console.log('values is ' + x + y));
 //     switchMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
 
+//This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
+//and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
+//time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
+//observer and starts emiting values again starting from zero but continues until its complete.
 
-
-
-
-
-// Challenge 8 MergeMap challenge be able to explain the output and the difference from merge map [Medium]
+// Challenge 8 MergeMap challenge be able to explain the output and the difference from switch map [Medium]
 // const character = new Observable((observer) => {
 //     observer.next('A');
 //     setTimeout(() => observer.next('B'), 200);
@@ -85,11 +95,17 @@ values.subscribe(x, y => console.log('values is ' + x + y));
 //     mergeMap(() => number)
 // ).subscribe((val) => console.log(`emitted value: ${val}`), null, () => console.log('complete'));
 
-
+//This is explaining that an observer is going to emit 5 numbers(in this case starting from 0)
+//and it will take 50(fractions of a sec) to emit before moving to the next number. But when the
+//time reaches 200(fractions of a sec) it stops emiting for the first observer and goes to the next
+//observer and picks up were the first observer left off while starting the next observer, emiting
+//values until its complete.
 
 // Challenge 9 use streamOfNumbers and only emit values when they are different from the previous value. [Easy]
 
-
+// streamOfNumbers.pipe(distinct()).subscribe(x => console.log(x));
 
 // Challenge 10 use streamOfNumbers but stop taking values after the number 4 has been emitted [Easy]
 
+// const untilFour = streamOfNumbers.pipe(take(4));
+// untilFour.subscribe(x => console.log(x));
